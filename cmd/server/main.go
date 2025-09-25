ae9de516ecc5b773267b9be8211614eea923dfae
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-09-25 21:48:18 +0200

    Create entry file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,28 @@
-package server
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/ivugurura/ivugurura-radio/config"
+	"github.com/ivugurura/ivugurura-radio/internal/stream"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+
+	manager := stream.NewManager()
+
+	manager.RegisterStudio("reformation-rw")
+	manager.RegisterStudio("reformantion-congo")
+
+	http.HandleFunc("/studios", func(w http.ResponseWriter, r *http.Request) {
+		manager.RouteStudioRequest(w, r)
+	})
+
+	log.Printf("Streaming server running at %s\n", cfg.ListenAddr)
+
+	if err := http.ListenAndServe(cfg.ListenAddr, nil); err != nil {
+		log.Fatal("Server failed ", err)
+	}
+}
