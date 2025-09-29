4d44489c3e383e2a8946bd78b6dc46e7ab49e9f1
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-09-29 18:38:56 +0200

    Declare listener types

---
+++ internal/listeners/types.go
@@ -0,0 +1,37 @@
+package listeners
+
+import (
+	"net"
+	"sync/atomic"
+	"time"
+)
+
+type Listener struct {
+	ID       string
+	StudioId string
+
+	// Connection metadata
+	ConnectedAt    time.Time
+	DisconnectedAt time.Time
+
+	// Network / Client
+	RemoteIP   net.IP
+	IPHash     string
+	Country    string
+	Region     string
+	City       string
+	Lat, Lon   float64
+	UserAgent  string
+	ClientType string
+
+	// Stats
+	ByteSent      atomic.Int64
+	LastHeartbeat atomic.Pointer[time.Time]
+
+	// Internal flags
+	Enriched atomic.Bool
+}
+
+func (l *Listener) MarkDisconnected() {
+	l.DisconnectedAt = time.Now()
+}
